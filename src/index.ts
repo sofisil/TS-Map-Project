da2b56c282d8e150fd0c1b8e5cb2ef203a28a2a0
sofisil <sofiamariasilvagimenez@gmail.com>
2022-11-22 11:48:46 -0300

    started working with show the map

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,26 @@
-import { faker } from '@faker-js/faker';
-import { Person } from './Person';
-import { Passenger } from './Passenger';
-import { Driver } from './Driver';
+/**
+  import { Passenger } from './Passenger';
+  import { Driver } from './Driver';
 
-const p = new Passenger();
-console.log(p);
+  const p = new Passenger();
+  console.log(p);
 
-const d = new Driver();
-console.log(d);
+  const d = new Driver();
+  console.log(d);
+ */
+
+/**
+  new google.maps.Map(
+    document.getElementById("map"),
+    {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      }
+    }
+  );
+ */
+
+import { Map } from "./Map"
+  var map = new Map("map");
